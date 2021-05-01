5630e62f30355b5f6b907997f406070603d01518
Nate Griggs <nateghsc09@gmail.com>
2021-05-01 14:01:20 -0500

    set up

--- server.js
+++ server.js
@@ -2,21 +2,49 @@
 const mysql = require("mysql");
 const inquirer = require("inguirer");
 const consoleTable = require("console.table");
-const Connection = require("mysql/lib/Connection");
-const { start } = require("node:repl");
 
 //Server Concections
 
 const concection = mysql.creatConnection({
-host: "localhost",
-port: 8080,
-user: "root",
-password: "mySQLPassword9",
-datebase: "tracker_DB"
+    host: "localhost",
+    port: 8080,
+    user: "root",
+    password: "mySQLPassword9",
+    datebase: "tracker_DB"
 });
 
-Connection.connect(function(err) {
-    if(err) throw err;
+Connection.connect(function (err) {
+    if (err) throw err;
     start();
+});
+
+//setting up inquirer
+
+function start() {
+    inquirer
+    .prompt({
+        type: "list",
+        name: "option",
+        message: "What would you like to do?",
+        choices:[
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "View Department",
+            "View Role",
+            "View Employee",
+            "Update Employee Roles",
+            "Update Employee Manager",
+            "Remove Employee",
+            "Remove Role",
+            "Remove Department",
+            
+
+
+
+
+        ]
+    })
 }
-);
+
+
